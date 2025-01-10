0f5407f365d87fef85058fded91121fd1a8c028f
MayureshNehe <mayuresh@iagentpro.com>
2025-01-10 17:10:04 +0530

    Update offsets.py

--- offsets.py
+++ offsets.py
@@ -1,18 +1,1 @@
 
-dwEntityList = 27350984
-dwLocalPlayerController = 27680624
-dwLocalPlayerPawn = 25599368
-dwViewAngles = 27830288
-
-
-m_bIsScoped = 9192
-m_iHealth = 836
-m_iFOVStart = 532
-m_vOldOrigin = 4900
-m_vecVelocity = 1024
-m_vecViewOffset = 3248
-m_hActiveWeapon = 88
-m_iItemDefinitionIndex = 442
-m_iClip1 = 5752
-m_iObserverMode = 64
-m_hObserverTarget = 68
